b303f0fc453ab22e1d0384cf1500c5d1986e924b
Mathankumar P <Mathankumar1127@gmail.com>
2023-09-04 20:51:23 +0530

    Hello World SpringBoot App

---
+++ example_18/src/test/java/com/springexercise/eazyschool/Example18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springexercise.eazyschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
