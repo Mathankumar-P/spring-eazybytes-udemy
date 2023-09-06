522ca50272bd6e9fc48d085fef971d641874a351
Mathankumar P <Mathankumar1127@gmail.com>
2023-09-06 16:47:08 +0530

    form validations using javaBean  & Hibernate Validatters

---
+++ example_24/src/main/java/com/springexercise/eazyschool/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.springexercise.eazyschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value={"","/","/home"})
+    public String displayHomePage(Model model) {
+        return "home.html";
+    }
+}
