fe4a2c791125b6fd55a366206e0f21d16a390e8e
Mathankumar P <Mathankumar1127@gmail.com>
2023-09-08 14:58:35 +0530

    Custom Security Configurations for public pages & CSRF Disable

---
+++ example_28/src/main/java/com/springexercise/eazyschool/config/ProjectSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.springexercise.eazyschool.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        //PermitAll
+        http.csrf().disable().
+                authorizeHttpRequests().requestMatchers("","/","/home").permitAll()
+                .requestMatchers("/holidays/**").permitAll()
+                .requestMatchers("/contact").permitAll()
+                .requestMatchers("/saveMessage").permitAll()
+                .requestMatchers("/courses").permitAll()
+                .requestMatchers("/about").permitAll()
+                .requestMatchers("/assets/**").permitAll().
+                and().formLogin()
+                .and().httpBasic();
+        return http.build();
+
+    }
+}
