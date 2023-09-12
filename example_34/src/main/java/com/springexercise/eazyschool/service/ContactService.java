6686beb3c5e889f785942b69be1689721b4e4ac7
Mathankumar P <Mathankumar1127@gmail.com>
2023-09-12 13:11:20 +0530

    Migrating from H2 to Remote freesqlhosting.net

---
+++ example_34/src/main/java/com/springexercise/eazyschool/service/ContactService.java
@@ -0,0 +1,50 @@
+package com.springexercise.eazyschool.service;
+
+import com.springexercise.eazyschool.constants.EazySchoolConstants;
+import com.springexercise.eazyschool.controller.ContactController;
+import com.springexercise.eazyschool.model.Contact;
+import com.springexercise.eazyschool.repository.ContactRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@Service
+public class ContactService {
+    @Autowired
+    private ContactRepository contactRepository;
+    public ContactService(){
+        System.out.println("Contact Service Bean initialized");
+    }
+    public boolean saveMessageDetails(Contact contact){
+        boolean isSaved = false;
+        contact.setStatus(EazySchoolConstants.OPEN);
+        contact.setCreated_by(EazySchoolConstants.ANONYMOUS);
+        contact.setCreated_At(LocalDateTime.now());
+        int result = contactRepository.saveMessageDetails(contact);
+        if(result>0){
+            isSaved =true;
+        }
+        return isSaved;
+    }
+
+    public List<Contact> findMsgWithOpenStatus() {
+        List<Contact>contactList = contactRepository.findMsgWithStatus(EazySchoolConstants.OPEN);
+        return contactList;
+    }
+
+    public boolean updateMsgStatus(int id, String name) {
+        boolean isUpdated = false;
+        int result = contactRepository.updateMessageStatus(id,EazySchoolConstants.CLOSE,name) ;
+        if(result>0) isUpdated=true;
+        return isUpdated;
+    }
+}
