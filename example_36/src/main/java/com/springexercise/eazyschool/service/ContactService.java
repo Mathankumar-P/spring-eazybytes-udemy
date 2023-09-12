5f31a31240fd87c67ac0631fb0e8e69d62c73547
Mathankumar P <Mathankumar1127@gmail.com>
2023-09-12 15:45:46 +0530

    Spring JPA Auditing using CreatedBy,LastModifiedBy,CreatedDate,LastModifiedDate

---
+++ example_36/src/main/java/com/springexercise/eazyschool/service/ContactService.java
@@ -0,0 +1,57 @@
+package com.springexercise.eazyschool.service;
+
+import com.springexercise.eazyschool.constants.EazySchoolConstants;
+import com.springexercise.eazyschool.controller.ContactController;
+import com.springexercise.eazyschool.model.Contact;
+import com.springexercise.eazyschool.repository.ContactRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class ContactService {
+    @Autowired
+    private ContactRepository contactRepository;
+    public ContactService(){
+        System.out.println("Contact Service Bean initialized");
+    }
+    public boolean saveMessageDetails(Contact contact){
+        boolean isSaved = false;
+        contact.setStatus(EazySchoolConstants.OPEN);
+        contact.setCreated_by(EazySchoolConstants.ANONYMOUS);
+        contact.setCreated_At(LocalDateTime.now());
+        contact= contactRepository.save(contact);
+        if(contact != null && contact.getContact_id()>0){
+            isSaved =true;
+        }
+        return isSaved;
+    }
+
+    public List<Contact> findMsgWithOpenStatus() {
+        List<Contact>contactList = contactRepository.findByStatus(EazySchoolConstants.OPEN);
+        return contactList;
+    }
+
+    public boolean updateMsgStatus(int contact_id) {
+        boolean isUpdated = false;
+        Optional<Contact>contact =contactRepository.findById(contact_id);
+        contact.ifPresent(contact1 -> {
+            contact1.setStatus(EazySchoolConstants.CLOSE);
+        });
+        Contact updateContact  = contactRepository.save(contact.get());
+        if(updateContact != null && updateContact.getUpdated_by()!=null){
+            isUpdated =true;
+        }
+        return isUpdated;
+    }
+}
---
+++ example_36/src/test/java/com/springexercise/eazyschool/EazySchoolApplicationTest.java
@@ -0,0 +1,13 @@
+package com.springexercise.eazyschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazySchoolApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
